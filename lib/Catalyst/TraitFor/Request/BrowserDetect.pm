8aacdde5c72836d9551987b3a4a9de9bd2894ab0
Florian Ragwitz <rafl@debian.org>
2009-08-10 08:25:20 +0200

    Add some docs.

--- lib/Catalyst/TraitFor/Request/BrowserDetect.pm
+++ lib/Catalyst/TraitFor/Request/BrowserDetect.pm
@@ -1,8 +1,41 @@
 package Catalyst::TraitFor::Request::BrowserDetect;
+# ABSTRACT: Browser detection for Catalyst::Requests
 
 use Moose::Role;
 use aliased 'HTTP::BrowserDetect';
 use namespace::autoclean;
+
+
+=head1 SYNOPSIS
+
+    package MyApp;
+
+    use Moose;
+    use namespace::autoclean;
+
+    use Catalyst;
+    use CatalystX::RoleApplicator;
+
+    extends 'Catalyst';
+
+    __PACKAGE__->apply_request_class_roles(qw/
+        Catalyst::TraitFor::Request::BrowserDetect
+    /);
+
+    __PACKAGE__->setup;
+
+=head1 DESCRIPTION
+
+Extend request objects with a method for browser detection.
+
+=attr browser
+
+    my $browser = $ctx->request->browser;
+
+Returns an C<HTTP::BrowserDetect> instance for the request. This allows you to
+get information about the client's user agent.
+
+=cut
 
 has browser => (
     is      => 'ro',
